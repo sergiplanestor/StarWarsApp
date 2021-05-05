22b06497eab35b9f11df6d73efe2e06e8175eacc
Sergi <joseprf@ext.bershka.com>
2021-05-05 19:50:21 +0200

    Splash activity setup

--- presentation/src/main/java/com/revolhope/presentation/feature/splash/SplashActivity.kt
+++ presentation/src/main/java/com/revolhope/presentation/feature/splash/SplashActivity.kt
@@ -8,14 +8,19 @@ import com.revolhope.presentation.library.base.BaseActivity
 
 class SplashActivity : BaseActivity<ActivitySplashBinding>() {
 
+    companion object {
+        private const val SPLASH_DELAY = 2500L // 2.5s
+    }
+
     override val binding: ActivitySplashBinding
         get() = ActivitySplashBinding.inflate(layoutInflater)
 
 
     override fun bindViews() {
         super.bindViews()
-
-        // FIXME: Dummy code. Replace
-        Handler(Looper.getMainLooper()).postDelayed({ DashboardActivity.start(this) }, 3000/* 3s */)
+        Handler(Looper.getMainLooper()).postDelayed(
+                { DashboardActivity.start(this@SplashActivity) },
+                SPLASH_DELAY
+        )
     }
 }
