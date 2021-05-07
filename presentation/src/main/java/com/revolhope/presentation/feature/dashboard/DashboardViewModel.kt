6a7db23040c96642fd9557214bf207233d5fd060
Sergi <joseprf@ext.bershka.com>
2021-05-08 01:05:22 +0200

    Added methods to fetch search results

--- presentation/src/main/java/com/revolhope/presentation/feature/dashboard/DashboardViewModel.kt
+++ presentation/src/main/java/com/revolhope/presentation/feature/dashboard/DashboardViewModel.kt
@@ -4,8 +4,15 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.revolhope.domain.feature.film.model.FilmModel
 import com.revolhope.domain.feature.film.usecase.FetchFilmsUseCase
+import com.revolhope.domain.feature.search.model.CharacterModel
+import com.revolhope.domain.feature.search.model.PlanetModel
 import com.revolhope.domain.feature.search.model.SearchTypeModel
+import com.revolhope.domain.feature.search.model.SpecieModel
+import com.revolhope.domain.feature.search.usecase.FetchCharactersByIdsUseCase
+import com.revolhope.domain.feature.search.usecase.FetchCharactersByNameUseCase
+import com.revolhope.domain.feature.search.usecase.FetchPlanetsByNameUseCase
 import com.revolhope.domain.feature.search.usecase.FetchSearchTypeUseCase
+import com.revolhope.domain.feature.search.usecase.FetchSpeciesByNameUseCase
 import com.revolhope.domain.feature.search.usecase.UpdateSearchTypeUseCase
 import com.revolhope.presentation.library.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -15,7 +22,11 @@ import javax.inject.Inject
 class DashboardViewModel @Inject constructor(
     private val fetchFilmsUseCase: FetchFilmsUseCase,
     private val fetchSearchTypeUseCase: FetchSearchTypeUseCase,
-    private val updateSearchTypeUseCase: UpdateSearchTypeUseCase
+    private val updateSearchTypeUseCase: UpdateSearchTypeUseCase,
+    private val fetchCharactersByNameUseCase: FetchCharactersByNameUseCase,
+    private val fetchCharactersByIdsUseCase: FetchCharactersByIdsUseCase,
+    private val fetchSpeciesByNameUseCase: FetchSpeciesByNameUseCase,
+    private val fetchPlanetsByNameUseCase: FetchPlanetsByNameUseCase
 ) : BaseViewModel() {
 
     private lateinit var originalFilms: List<FilmModel>
@@ -25,6 +36,18 @@ class DashboardViewModel @Inject constructor(
 
     private val _searchTypeLiveData = MutableLiveData<SearchTypeModel?>()
     val searchTypeLiveData: LiveData<SearchTypeModel?> get() = _searchTypeLiveData
+
+    private val _charactersByIdsLiveData = MutableLiveData<List<CharacterModel>>()
+    val charactersByIdsLiveData: LiveData<List<CharacterModel>> get() = _charactersByIdsLiveData
+
+    private val _charactersByNameLiveData = MutableLiveData<List<CharacterModel>>()
+    val charactersByNameLiveData: LiveData<List<CharacterModel>> get() = _charactersByNameLiveData
+
+    private val _speciesByNameLiveData = MutableLiveData<List<SpecieModel>>()
+    val speciesByNameLiveData: LiveData<List<SpecieModel>> get() = _speciesByNameLiveData
+
+    private val _planetsByNameLiveData = MutableLiveData<List<PlanetModel>>()
+    val planetsByNameLiveData: LiveData<List<PlanetModel>> get() = _planetsByNameLiveData
 
     fun fetchFilms() {
         launchAsync(
@@ -51,8 +74,40 @@ class DashboardViewModel @Inject constructor(
 
     fun applyFilmFilter(query: String) {
         if (::originalFilms.isInitialized) {
-            _filmsLiveData.value =
-                originalFilms.apply { if (query.isNotBlank()) filter { it.title.contains(query) } }
+            _filmsLiveData.value = if (query.isBlank()) {
+                originalFilms
+            } else {
+                originalFilms.filter { it.title.contains(query, ignoreCase = true) }
+            }
         }
     }
+
+    fun fetchCharactersByName(query: String) {
+        launchAsync(
+            asyncTask = { fetchCharactersByNameUseCase.invoke(FetchCharactersByNameUseCase.Params(query)) },
+            onSuccess = _charactersByNameLiveData::setValue
+        )
+    }
+
+    fun fetchCharactersByIds(ids: List<Int>) {
+        launchAsync(
+            asyncTask = { fetchCharactersByIdsUseCase.invoke(FetchCharactersByIdsUseCase.Params(ids)) },
+            onSuccess = _charactersByIdsLiveData::setValue
+        )
+    }
+
+    fun fetchSpeciesByName(query: String) {
+        launchAsync(
+            asyncTask = { fetchSpeciesByNameUseCase.invoke(FetchSpeciesByNameUseCase.Params(query)) },
+            onSuccess = _speciesByNameLiveData::setValue
+        )
+    }
+
+    fun fetchPlanetsByName(query: String) {
+        launchAsync(
+            asyncTask = { fetchPlanetsByNameUseCase.invoke(FetchPlanetsByNameUseCase.Params(query)) },
+            onSuccess = _planetsByNameLiveData::setValue
+        )
+    }
+
 }
